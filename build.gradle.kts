5a44af77ec94f66b3a7976748ea9287f18f95731
Turbovadim <yolkin.vadim@gmail.com>
2024-04-28 23:35:19 +0300

    Update dependencies and add H2 driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.ir.backend.js.compile
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -24,18 +23,20 @@ val deps = dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 
     // Exposed
-    api("org.jetbrains.exposed:exposed-core:0.48.0")
-    api("org.jetbrains.exposed:exposed-dao:0.48.0")
-    api("org.jetbrains.exposed:exposed-jdbc:0.48.0")
+    api("org.jetbrains.exposed:exposed-core:0.49.0")
+    api("org.jetbrains.exposed:exposed-dao:0.49.0")
+    api("org.jetbrains.exposed:exposed-jdbc:0.49.0")
+
+    api("com.zaxxer:HikariCP:5.1.0")
 
     api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
-    api("com.charleskorn.kaml:kaml:0.58.0")
+    api("com.charleskorn.kaml:kaml:0.59.0")
 
     // Database drivers
     runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
     runtimeOnly("com.impossibl.pgjdbc-ng:pgjdbc-ng:0.8.9")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.1")
-
+    runtimeOnly("com.h2database:h2:2.2.224")
 }
 
 publishing {
