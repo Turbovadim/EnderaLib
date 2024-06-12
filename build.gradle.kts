7706c7c9cc1bce23516682f2b3c00ef3cdbac69c
Turbovadim <yolkin.vadim@gmail.com>
2024-06-13 00:50:24 +0300

    Update jitpack.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     `maven-publish`
 }
@@ -17,15 +17,16 @@ repositories {
 
 val deps = dependencies {
     // Minecraft APIs
+    val exposedVersion = "0.50.1"
 
     compileOnly("net.kyori:adventure-text-minimessage:4.16.0")
 
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 
     // Exposed
-    api("org.jetbrains.exposed:exposed-core:0.49.0")
-    api("org.jetbrains.exposed:exposed-dao:0.49.0")
-    api("org.jetbrains.exposed:exposed-jdbc:0.49.0")
+    api("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 
     api("com.zaxxer:HikariCP:5.1.0")
 
@@ -33,7 +34,7 @@ val deps = dependencies {
     api("com.charleskorn.kaml:kaml:0.59.0")
 
     // Database drivers
-    runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
+    runtimeOnly("com.mysql:mysql-connector-j:8.4.0")
     runtimeOnly("com.impossibl.pgjdbc-ng:pgjdbc-ng:0.8.9")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.1")
     runtimeOnly("com.h2database:h2:2.2.224")
@@ -64,14 +65,17 @@ val generatePluginYml = tasks.create("generatePluginYml", Copy::class.java) {
     from("/src/main/templates/plugin.yml")
     into("/src/main/resources")
     expand(mapOf("projectVersion" to project.version))
+    outputs.upToDateWhen { false }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+kotlin {
+    compilerOptions {
+        apiVersion.set(KotlinVersion.KOTLIN_1_7)
+    }
 }
 
 tasks.withType<JavaCompile> {
