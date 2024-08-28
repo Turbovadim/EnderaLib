77ac4111f37930675aebc89e94348e2226ecc311
Turbovadim <yolkin.vadim@gmail.com>
2024-08-28 23:11:26 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
+    id("com.gradleup.shadow") version "8.3.0"
     `maven-publish`
 }
 
 group = "org.endera"
-version = "1.0.1"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -17,7 +17,7 @@ repositories {
 
 val deps = dependencies {
     // Minecraft APIs
-    val exposedVersion = "0.52.0"
+    val exposedVersion = "0.53.0"
 
     compileOnly("net.kyori:adventure-text-minimessage:4.16.0")
 
@@ -31,13 +31,13 @@ val deps = dependencies {
     api("com.zaxxer:HikariCP:5.1.0")
 
     api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.7.1")
-    api("com.charleskorn.kaml:kaml:0.60.0")
+    api("com.charleskorn.kaml:kaml:0.61.0")
 
     // Database drivers
-    runtimeOnly("com.mysql:mysql-connector-j:8.4.0")
+    runtimeOnly("com.mysql:mysql-connector-j:9.0.0")
     runtimeOnly("com.impossibl.pgjdbc-ng:pgjdbc-ng:0.8.9")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.4.0")
-    runtimeOnly("com.h2database:h2:2.2.224")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.4.1")
+    runtimeOnly("com.h2database:h2:2.3.232")
 }
 
 publishing {
