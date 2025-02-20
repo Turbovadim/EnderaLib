5dd22a40c0c09ae9a185505baa6bc02206817b73
Yolkin Vadim <yolkin.vadim@gmail.com>
2025-02-20 20:10:47 +0300

    remove comment changes

--- src/main/kotlin/org/endera/enderalib/config/libConfiguration.kt
+++ src/main/kotlin/org/endera/enderalib/config/libConfiguration.kt
@@ -1,24 +1,15 @@
 package org.endera.enderalib.config
 
 import kotlinx.serialization.Serializable
-import org.endera.enderalib.utils.configuration.Comment
 
 @Serializable
 internal data class ConfigScheme(
-    @Comment("""
-        This message will be thrown when Player.checkPermission() is used
-        This message will be thrown when Player.checkPermission() is used2
-        This message will be thrown when Player.checkPermission() is used3
-    """)
     val messages: MessagesSchema = MessagesSchema(),
-    @Comment("This message will be thrown when Player.checkPermission() is used 228")
-    val sexSexer: String = "sosal?",
 )
 
 @Serializable
 internal data class MessagesSchema(
     val prefix: String = "<gradient:#5e4fa2:#f79459>[EnderaLib]</gradient>",
-    @Comment("This message will be thrown when Player.checkPermission() is used")
     val noPermission: String = "{prefix} You don't have permission to do this.",
 )
 
